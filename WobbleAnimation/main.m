835b769aaff382c75995c491976ccecf33b64a54
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-06-25 15:15:27 -0700

    Initial Commit

---
+++ WobbleAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WobbleAnimation
+//
+//  Created by Jialiang Xiang on 2015-06-25.
+//  Copyright (c) 2015 Jialiang Xiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
